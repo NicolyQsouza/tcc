e42bc9092f52dde505121e7bfa0a8677f350d644
NicolyLinda <134062246+NicolyQsouza@users.noreply.github.com>
2025-01-29 23:38:27 -0300

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const router = express.Router();  // Criação do roteador
+const router = express.Router();  
 
-const authController = require('../controllers/authController');  // Importando o controlador de autenticação
+const authController = require('../controllers/authController');  
 
 // Rotas de autenticação
 router.get('/login', authController.renderLoginForm);
+router.get('/login2', authController.renderLoginForm2);  
 router.post('/login', authController.login);
+router.post('/login2', authController.login);
 router.get('/logout', authController.logout);
 router.post('/createUser', authController.createUser);
 
-module.exports = router;  // Exportando o roteador
+module.exports = router;  
